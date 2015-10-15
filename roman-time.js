ae56fe3cd0e85ee05f91976b1f4544dba0909e18
KladovValery <valerijkladov@yandex.ru>
2015-10-15 23:11:56 +0500

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -1,55 +1,29 @@
 var hours = process.argv[2];
 var minutes = process.argv[3];
-function RomanTime (number) {
-	switch (number) {
-		default:
-			return "";
-		case 1:
-			return "I";
-			break;
-		case 2:
-			return "II";
-			break;
-		case 3:
-			return "III";
-			break;
-		case 4:
-			return "IV";
-			break;
-		case 5:
-			return "V";
-			break;
-		case 6:
-			return "VI";
-			break;
-		case 7:
-			return "VII";
-			break;
-		case 8:
-			return "VIII";
-			break;	
-		case 9:
-			return "IX";
-			break;
-		case 10:
-			return "X";
-			break;
-		case 20:
-			return "XX";
-			break;
-		case 30:
-			return "XXX";
-			break;
-		case 40:
-			return "XL";
-			break;
-		case 50:
-			return "L";
-			break;
-			}
-	}
-if (hours >= 0 && hours < 24 && minutes >= 0 && minutes < 60)
+var numbers = {
+    0: '',
+    1: 'I',
+    2: 'II',
+    3: 'III',
+    4: 'IV',
+    5: 'V',
+    6: 'VI',
+    7: 'VII',
+    8: 'VIII',
+    9: 'IX',
+    10: 'X',
+    20: 'XX',
+    30: 'XXX',
+    40: 'XL',
+    50: 'X'
+}
+
+if (hours >= 0 && hours < 24 && minutes >= 0 && minutes < 60  && Math.floor(hours) == hours && Math.floor(minutes) == minutes)
 {
-	console.log("%s%s:%s%s", RomanTime(Math.floor(hours / 10)* 10), RomanTime(hours % 10), RomanTime(Math.floor(minutes / 10)* 10), RomanTime(minutes % 10));
-	}
-else console.log("Время указано не верно");	
+	console.log("%s%s:%s%s",
+	            numbers[(Math.floor(hours / 10)* 10)],
+	            numbers[(hours % 10)],
+	            numbers[(Math.floor(minutes / 10)* 10)],
+	            numbers[(minutes % 10)]);
+}
+else console.log("Время указано не верно");
